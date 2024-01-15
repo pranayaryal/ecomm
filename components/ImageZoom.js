c2c3a545f3733b614f0cca9157b28b04fbe0317e
pranayaryal <drpranayaryal@gmail.com>
2024-01-15 11:31:45 -0500

    more changes

--- components/ImageZoom.js
+++ components/ImageZoom.js
@@ -3,60 +3,132 @@ import { useRef, useEffect, useState } from 'react'
 
 export default function ImageZoom() {
   const [cursorPos, setCursorPos] = useState({})
+  const [showLens, setShowLens] = useState(false)
 
   const imageRef = useRef(null)
   const lensRef = useRef(null)
   const getCursor = () => {
     // let e = window.event
     let bounds = imageRef.getBoudingClientRect()
-    console.log(bounds)
   }
+
+  const onMouseLeftImage = () => {
+    lensRef.current.style.backgroundImage = null;
+    lensRef.current.style.height = 0;
+    lensRef.current.style.width = 0;
+
+  }
+
+  const onImageHover = () => {
+      if (!imageRef.current?.contains(event.target)) {
+        return
+      }
+      const ratio = 3;
+      // setLensDims({x: imageRef.current.width, y: imageRef.current.height})
+      lensRef.current.style.height = imageRef.current.style.height
+      lensRef.current.style.width = imageRef.current.style.width
+      lensRef.current.style.backgroundImage = `url(${imageRef.current.src})`
+      let bounds = imageRef.current.getBoundingClientRect()
+      let cursorX = event.pageX - bounds.left
+      let cursorY = event.pageY - bounds.top
+
+      cursorX = cursorX - window.scrollX
+      cursorY = cursorY - window.scrollY
+
+      cursorX = Math.max(0, cursorX)
+      cursorY = Math.max(0, cursorY)
+      if (cursorX === 0) {
+        lensRef.current.backgroundImage = null;
+      }
+
+      console.log(lensRef.current.offsetWidth)
+
+
+      // let positionLeft = cursorX - (lensRef.current.offsetWidth / 2)
+      // let positionTop = cursorY - (lensRef.current.offsetHeight / 2)
+
+      // positionLeft = Math.max(positionLeft, 0)
+      // positionTop = Math.max(positionTop, 0)
+      console.log(imageRef.current.height)
+
+
+      // positionTop = Math.min(positionTop, (imageRef.current.height - lensRef.current.offsetHeight / 3))
+      // positionLeft = Math.min(positionLeft, imageRef.current.width - lensRef.current.offsetWidth / 3)
+
+      // lensRef.current.style.left = `${positionLeft}px`
+      // lensRef.current.style.top = `${positionTop}px`
+
+      lensRef.current.style.backgroundPosition = `-${(cursorX * ratio)}px -${(cursorY * ratio)}px`;
+
+  }
+
 
 
   useEffect(() => {
 
     const handleImageHover = (event) => {
-      if (imageRef.current?.contains(event.target)) {
-        const ratio = 5;
-        let bounds = imageRef.current.getBoundingClientRect()
-        let cursorX = event.pageX - bounds.left
-        let cursorY = event.pageY - bounds.top
-        let positionLeft = cursorX - (lensRef.current.offsetWidth / 2)
-        let positionTop = cursorY - (lensRef.current.offsetHeight / 2)
-
-        positionLeft = Math.max(positionLeft, 0)
-        positionTop = Math.max(positionTop, 0)
-
-        if (positionTop > imageRef.current.height - lensRef.current.offsetHeight / 3) {
-          positionTop = imageRef.current.height - lensRef.current.offsetHeight / 3
-        }
-
-        if (positionLeft > imageRef.current.width - lensRef.current.offsetWidth / 3) {
-          positionLeft = imageRef.current.width - lensRef.current.offsetWidth / 3
-        }
-
-        lensRef.current.style.left = positionLeft + 'px'
-        lensRef.current.style.top = positionTop + 'px'
-
-        lensRef.current.style.backgroundPosition = `-${(cursorX * ratio)}px -${(cursorY * ratio)}px`;
-        lensRef.current.style.backgroundImage = `url(${imageRef.current.src})`
-        // lensRef.current.style.backgroundImage = `url('/shoe1.jpg')`
-
+      if (!imageRef.current?.contains(event.target)) {
+        return
       }
+      const ratio = 2;
+      // setLensDims({x: imageRef.current.width, y: imageRef.current.height})
+      lensRef.current.style.backgroundImage = `url(${imageRef.current.src})`
+      let bounds = imageRef.current.getBoundingClientRect()
+      let cursorX = event.pageX - bounds.left
+      let cursorY = event.pageY - bounds.top
+
+      cursorX = cursorX - window.scrollX
+      cursorY = cursorY - window.scrollY
+
+      cursorX = Math.max(0, cursorX)
+      cursorY = Math.max(0, cursorY)
+      if (cursorX === 0) {
+        lensRef.current.backgroundImage = null;
+      }
+
+      console.log(lensRef.current.offsetWidth)
+
+
+      // let positionLeft = cursorX - (lensRef.current.offsetWidth / 2)
+      // let positionTop = cursorY - (lensRef.current.offsetHeight / 2)
+
+      // positionLeft = Math.max(positionLeft, 0)
+      // positionTop = Math.max(positionTop, 0)
+      console.log(imageRef.current.height)
+
+
+      // positionTop = Math.min(positionTop, (imageRef.current.height - lensRef.current.offsetHeight / 3))
+      // positionLeft = Math.min(positionLeft, imageRef.current.width - lensRef.current.offsetWidth / 3)
+
+      // lensRef.current.style.left = `${positionLeft}px`
+      // lensRef.cu/rrent.style.top = `${positionTop}px`
+
+      lensRef.current.style.backgroundPosition = `-${(cursorX * ratio)}px -${(cursorY * ratio)}px`;
+
     };
-    window.addEventListener('mousemove', handleImageHover)
-    return () => window.removeEventListener('mousemove', handleImageHover)
+    // window.addEventListener('mousemove', handleImageHover)
+    return () => {
+      window.removeEventListener('mousemove', handleImageHover)
+    }
   }, [])
 
 
 
   return (
-    <div id="img-container" className='relative z-10 max-w-[800px]'>
-      <div id="lens" ref={lensRef} className='absolute z-20 h-[125px] w-[125px] border-2 border-black cursor-none'></div>
-      <img
-        className='pointer border border-gray-700'
-        id="featured" ref={imageRef} src="/preset1.png" />
+    <>
+      <div id="img-container" className='relative z-10 w-1/2'>
+        <img
+          onMouseMove={(e) => onImageHover()}
+          onMouseLeave={() => onMouseLeftImage()}
+          className='pointer border border-gray-700  object-cover'
+          id="featured" ref={imageRef} src="/shoe1.jpg" />
 
-    </div>
+      </div>
+      <div className='relative w-1/2'>
+        <div id="lens" ref={lensRef}
+          className={`bg-no-repeat z-20 object-cover w-[400px] h-[400px] cursor-none`}>
+        </div>
+      </div>
+    </>
   )
 }
--- pages/index.js
+++ pages/index.js
@@ -29,7 +29,7 @@ export default function Home() {
         </div>
 
       </div>
-      <div>
+      <div className='flex'>
         <ImageZoom />
 
       </div>
